f608e7e0619ab8d398c9cda0a7448ec731c8ccbc
Jozsef IVANGAFO <jivangafouwaterloo.ca>
2023-10-23 15:39:19 -0400

    TCP html request done

--- client.py
+++ client.py
@@ -1,10 +1,14 @@
 from socket import *
+import os
 serverIP = "127.0.0.1"
 serverPort = 11000
 clientSocket = socket(AF_INET, SOCK_STREAM)
 clientSocket.connect((serverIP, serverPort))
-sentence = input("Input lowercase sentence:")
-clientSocket.send(sentence.encode())
+
+
+file_name = input("Introduce file:")
+
+clientSocket.send(file_name.encode())
 modifiedSentence = clientSocket.recv(2048)
 print("From Server:", modifiedSentence.decode())
 clientSocket.close()
--- webserver.py
+++ webserver.py
@@ -9,6 +9,30 @@ while (True):
     connectionSocket, addr = serverSocket.accept()
     sentence = connectionSocket.recv(2048).decode()
     print ("sentence received")
-    capitalizedSentence = sentence.upper()
-    connectionSocket.send(capitalizedSentence.encode())
+    list_sentence=sentence.split()
+    # print(list_sentence)
+    print(sentence)
+    type_request=list_sentence[0]
+    file_name=list_sentence[1][1:]
+
+    
+    if type_request=="GET":
+        try:
+            print(file_name)
+            with open(file_name, "rb") as file:
+                # FIXME: chat gpt
+                file_content = file.read()
+                response = "HTTP/1.1 200 OK\r\n"
+                response += "Content-Length: " + str(len(file_content)) + "\r\n"
+                response += "\r\n"
+                response = response.encode() + file_content
+        except FileNotFoundError:
+            response = "HTTP/1.1 404 Not Found\r\n\r\nFile not found."
+        connectionSocket.send(response)
+
+
+    
+    # capitalizedSentence = file_content.upper()
+
+    # connectionSocket.send(capitalizedSentence.encode())
     connectionSocket.close()
